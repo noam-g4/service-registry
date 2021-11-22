22025b53d07de9e3c5685307f3c29b04180ffe5f
Noam Yadgar <noam@nyg4.local>
2021-11-22 16:42:23 +0200

    registry controller + moving to memory

---
+++ src/controllers/registry.js
@@ -0,0 +1,23 @@
+const semver = require('semver')
+const { getState, setState } = require('./state')
+
+const key = ({ name, version, ip, port }) => `${name}v${version}//${ip}:${port}`
+
+module.exports.register = service =>
+  setState(state => ({
+    ...state,
+    [key(service)]: { ...service, timestamp: Math.floor(new Date() / 1000) },
+  }))
+
+module.exports.unregister = service =>
+  setState(state => {
+    if (state[key(service)]) delete state[key(service)]
+    return state
+  })
+
+module.exports.get = (name, version) => {
+  const candidates = Object.values(getState()).filter(
+    s => s.name === name && semver.satisfies(s.version, version)
+  )
+  return candidates[Math.floor(Math.random() * candidates.length)]
+}
--- src/controllers/state.js
+++ src/controllers/state.js
@@ -1,30 +1,21 @@
-require('dotenv').config()
-const { readFile, writeFile } = require('fs/promises')
-const { join } = require('path')
+const state = {}
 
-const stateFilePath = join(__dirname, '../', '../', `/${process.env.STATE}`)
-
-const getState = async (path = stateFilePath) => {
-  try {
-    const file = await readFile(path)
-    return JSON.parse(file)
-  } catch (err) {
-    return []
-  }
-}
-
-const dispatch = async (action, path = stateFilePath) => {
-  try {
-    const state = await getState()
-    const newState = action(state)
-    await writeFile(path, JSON.stringify(newState, null, 4))
-    return newState
-  } catch (err) {
-    return err
-  }
+const setState = action => {
+  const update = action({ ...state })
+  Object.keys(state).forEach(key => {
+    if (!update[key]) return delete state[key]
+    Object.keys(state[key]).forEach(sKey => {
+      if (state[key][sKey] !== update[key][sKey])
+        return (state[key][sKey] = update[key][sKey])
+    })
+  })
+  Object.keys(update).forEach(key => {
+    if (!state[key]) state[key] = update[key]
+  })
+  return state
 }
 
 module.exports = {
-  getState,
-  dispatch,
+  getState: () => ({ ...state }),
+  setState,
 }
